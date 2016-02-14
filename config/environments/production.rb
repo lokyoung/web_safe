1b8118ded94a893821aba51119b3542a3e7f300a
lokyoung <lokyoung@outlook.com>
2016-02-14 09:21:54 +0800

    disable request foregery protection

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_cable.allowed_request_origins = ["http://192.241.193.66/cable"]
+
+  ActionCable.server.config.disable_request_forgery_protection = true
 end
